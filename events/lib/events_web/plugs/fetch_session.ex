fe7cda32b945eaa15e531b9e904050a875c44e52
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-11 21:09:16 -0500

    Update fetch_session.ex

--- events/lib/events_web/plugs/fetch_session.ex
+++ events/lib/events_web/plugs/fetch_session.ex
@@ -2,7 +2,7 @@ defmodule EventsWeb.Plugs.FetchSession do
   import Plug.Conn
 
   def init(args), do: args
-
+ 
   def call(conn, _args) do
     user = Events.Users.get_user(get_session(conn, :user_id) || -1)
     if user do
