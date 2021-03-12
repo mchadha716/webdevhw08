b8a83902771cb6081fe882d1ad63823260654594
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-11 21:00:47 -0500

    Update user.ex

--- events/lib/events/users/user.ex
+++ events/lib/events/users/user.ex
@@ -1,24 +1,21 @@
-defmodule EventsWeb.SessionControllerCHANGETHIS do
-  use EventsWeb, :controller
 
-  def create(conn, %{"name" => name}) do
-    user = Events.Users.get_user_by_name(name)
-    if user do
-      conn
-      |> put_session(:user_id, user.id)
-      |> put_flash(:info, "Welcome back #{user.name}")
-      |> redirect(to: Routes.page_path(conn, :index))
-    else
-      conn
-      |> put_flash(:error, "Login failed.")
-      |> redirect(to: Routes.page_path(conn, :index))
-    end
+defmodule Events.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    has_many :posts, Events.Posts.Post
+    has_many :comments, Events.Comments.Comment
+
+    timestamps()
   end
 
-  def delete(conn, _params) do
-    conn
-    |> delete_session(:user_id)
-    |> put_flash(:info, "Logged out.")
-    |> redirect(to: Routes.page_path(conn, :index))
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
   end
 end
