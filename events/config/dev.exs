fb8c3f64dc8452935756ea504e24770156a8285d
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-11 20:57:15 -0500

    Update dev.exs

--- events/config/dev.exs
+++ events/config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :events, Events.Repo,
-  username: "events_two",
-  password: "woojohKaeth8",
-  database: "events_two_prod",
+  username: "events",
+  password: "aiNg8tuSee3k",
+  database: "events_prod",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
