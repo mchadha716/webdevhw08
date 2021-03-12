76f2ce24cd5aa74800718520fe1654ecf28d7dfd
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-11 23:23:16 -0500

    Update events_web.ex

--- events/lib/events_web.ex
+++ events/lib/events_web.ex
@@ -2,16 +2,12 @@ defmodule EventsWeb do
   @moduledoc """
   The entrypoint for defining your web interface, such
   as controllers, views, channels and so on.
-
   This can be used in your application as:
-
       use EventsWeb, :controller
       use EventsWeb, :view
-
   The definitions below will be executed for every view,
   controller, etc, so keep them short and clean, focused
   on imports, uses and aliases.
-
   Do NOT define functions inside the quoted expressions
   below. Instead, define any helper function in modules
   and import those modules here.
@@ -23,6 +19,7 @@ defmodule EventsWeb do
 
       import Plug.Conn
       import EventsWeb.Gettext
+      import EventsWeb.Helpers
       alias EventsWeb.Router.Helpers, as: Routes
     end
   end
@@ -68,6 +65,7 @@ defmodule EventsWeb do
 
       import EventsWeb.ErrorHelpers
       import EventsWeb.Gettext
+      import EventsWeb.Helpers
       alias EventsWeb.Router.Helpers, as: Routes
     end
   end
