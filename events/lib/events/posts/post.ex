c2b5b0bdb936699b3f58514a666ac8ae857ce6b3
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-11 21:00:11 -0500

    Update post.ex

--- events/lib/events/posts/post.ex
+++ events/lib/events/posts/post.ex
@@ -3,16 +3,21 @@ defmodule Events.Posts.Post do
   import Ecto.Changeset
 
   schema "posts" do
-    field :body, :string
+    field :date, :string
+    field :description, :string
+    field :name, :string
+    field :score, :integer, virtual: true
     belongs_to :user, Events.Users.User
-
+    has_many :comments, Events.Comments.Comment
+    has_many :rsvps, Events.Rsvps.Rsvp
+    has_many :invites, Events.Invites.Invite
     timestamps()
   end
 
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:body, :user_id])
-    |> validate_required([:body, :user_id])
+    |> cast(attrs, [:name, :date, :description, :user_id])
+    |> validate_required([:name, :date, :description, :user_id])
   end
 end
