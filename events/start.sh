e61998c42fbfef86e71f5e2bdfc2923dd23c3b0d
mchadha716 <mchadha716@gmail.com>
2021-03-13 02:50:23 +0000

    failed to deploy :(

--- events/start.sh
+++ events/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MIX_ENV=prod
-export PORT=4799
+export PORT=4650
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export DATABASE_URL=ecto://"events_two":aiNg8tuSee3k@localhost/events_two_prod
 
